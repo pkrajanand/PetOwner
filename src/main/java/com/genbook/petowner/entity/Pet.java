0cc85a7ff69413392c1ec20987f5139053c1a278
RajPK <pkrajanand@gmail.com>
2018-10-25 00:19:01 +1100

    ensured only ownerId is returned for pets instead of whole owner object; also ensured to skip "new" hibernate property

--- src/main/java/com/genbook/petowner/entity/Pet.java
+++ src/main/java/com/genbook/petowner/entity/Pet.java
@@ -7,11 +7,18 @@ import javax.persistence.Table;
 
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
+@JsonIgnoreProperties("new")
 @Data
 @NoArgsConstructor
 @RequiredArgsConstructor
@@ -23,6 +30,9 @@ public class Pet extends AbstractPersistable<Long> {
   private String name;
   private String birthday;
 
+  @JsonIdentityInfo(generator=ObjectIdGenerators.PropertyGenerator.class, property="id")
+  @JsonIdentityReference(alwaysAsId=true)
+  @JsonProperty("ownerId")
   @ManyToOne(cascade=CascadeType.PERSIST)
   private Owner owner;
   
