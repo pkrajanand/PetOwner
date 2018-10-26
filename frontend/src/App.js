9fea01343e5c8453cfa614e4787b45bedf301696
RajPK <pkrajanand@gmail.com>
2018-10-26 18:53:14 +1100

    a very crude display of owners fetched from backend; to ensure the end-to-end flow is in place; now need to tune this UI a bit more to have in a presentable way

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,45 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    isLoading: true,
+    owners: []
+  };
+  async componentDidMount() {
+
+    const response = await fetch('/owners');
+    const body = await response.json();
+    this.setState({ owners: body, isLoading: false });
+  }
+
   render() {
+
+    const { owners, isLoading } = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">PetOwner 1.0</h1>
+          <div className="App-intro">
+            <h2>Owners</h2>
+
+            {owners.map(owner =>
+              <div>
+                <div key={owner.id}>
+                  {owner.id}
+                  {owner.firstName} {owner.lastName}
+                  {owner.city}
+                  {owner.petIds}
+                </div>
+              </div>
+            )}
+          </div>
         </header>
       </div>
     );
