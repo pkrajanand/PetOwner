411b9c8f0c5706e2a0c1541442cca0128a9592e6
RajPK <pkrajanand@gmail.com>
2018-10-28 19:39:39 +1100

    added UI test for add-pet feature

--- frontend/src/components/OwnerList.spec.js
+++ frontend/src/components/OwnerList.spec.js
@@ -40,6 +40,33 @@ describe('components', () => {
       const addPetButton = petsElementForOwner1.findByType(Button);
       expect(addPetButton.props.children).toBe('Add Pet');
 
+    
+
+    });
+
+    it('adding pet for an owner', () => {
+
+      const root = TestRenderer.create(<OwnerList/>).root;
+      const instance = root.instance;
+      instance.setState({
+        owners: [
+          { "firstName":"Jowena", "lastName":"Chua", "city":"Manila", "petIds":[1],"id":3 },
+          { "firstName":"Wally","lastName":"Crocker","city":"Sydney","petIds":[2],"id":2 },
+          { "firstName":"Monisha","lastName":"Suresh","city":"Bangalore","petIds":[3],"id":1 }
+        ],
+        pets: [],
+        isLoading: false
+      });
+
+      const ownerTableElement = root.findByProps({ className: "mt-4 owners" });
+      const ownerItems = ownerTableElement.findByType('tbody').children;      
+      const owner1Details = ownerItems[0].findByProps({ className: "mt-4" });      
+      const petsElementForOwner1 = owner1Details.findByType(PetList);
+      const addPetButton = petsElementForOwner1.findByType(Button);
+
+      petsElementForOwner1.props.onAddPet({id: 1, name: "parrot", birthday: "16/05/2017", ownerId: 3});
+
+      expect(instance.state.owners[0].petIds.length).toBe(2);
 
     });
   })
