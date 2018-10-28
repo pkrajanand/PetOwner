c704c4adc6ff317df0954e737c13305599282a9f
RajPK <pkrajanand@gmail.com>
2018-10-28 19:44:56 +1100

    covered front-end tests to fix https://github.com/pkrajanand/PetOwner/issues/2

--- frontend/src/components/OwnerList.spec.js
+++ frontend/src/components/OwnerList.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
-import {Button, Table } from 'reactstrap';
+import {Button, Table} from 'reactstrap';
 import OwnerList from './OwnerList';
 import PetList from './PetList';
 
@@ -14,7 +14,7 @@ function setup( editing = false ) {
 describe('components', () => {
   describe('OwnerList', () => {
 
-    it('initial render with owners without pets', () => {
+    it('initial render with owners without pets and verify owner-1 details', () => {
 
       const root = TestRenderer.create(<OwnerList/>).root;
       const instance = root.instance;
@@ -32,15 +32,75 @@ describe('components', () => {
       expect(ownerTableElement.type).toBe(Table);
       const ownerItems = ownerTableElement.findByType('tbody').children;
       expect(ownerItems.length).toBe(3);
-      
+
+      const owner1NameTd = ownerItems[0].children[1].children[0];
+      expect(owner1NameTd).toBe("Jowena Chua");
       const owner1Details = ownerItems[0].findByProps({ className: "mt-4" });
-      expect(owner1Details.type).toBe(Table);
+      expect(owner1Details.type).toBe(Table);      
       const petsElementForOwner1 = owner1Details.findByType(PetList);
       expect(petsElementForOwner1.children.length).toBe(1);
       const addPetButton = petsElementForOwner1.findByType(Button);
       expect(addPetButton.props.children).toBe('Add Pet');
 
-    
+    });
+
+    it('initial render with owners without pets and verify owner-2 details', () => {
+
+      const root = TestRenderer.create(<OwnerList/>).root;
+      const instance = root.instance;
+      instance.setState({
+        owners: [
+          { "firstName":"Jowena", "lastName":"Chua", "city":"Manila", "petIds":[],"id":-3 },
+          { "firstName":"Wally","lastName":"Crocker","city":"Sydney","petIds":[],"id":-2 },
+          { "firstName":"Monisha","lastName":"Suresh","city":"Bangalore","petIds":[],"id":-1 }
+        ],
+        pets: [],
+        isLoading: false
+      });
+
+      const ownerTableElement = root.findByProps({ className: "mt-4 owners" });
+      expect(ownerTableElement.type).toBe(Table);
+      const ownerItems = ownerTableElement.findByType('tbody').children;
+      expect(ownerItems.length).toBe(3);
+
+      const owner1NameTd = ownerItems[1].children[1].children[0];
+      expect(owner1NameTd).toBe("Wally Crocker");
+      const owner1Details = ownerItems[1].findByProps({ className: "mt-4" });
+      expect(owner1Details.type).toBe(Table);      
+      const petsElementForOwner1 = owner1Details.findByType(PetList);
+      expect(petsElementForOwner1.children.length).toBe(1);
+      const addPetButton = petsElementForOwner1.findByType(Button);
+      expect(addPetButton.props.children).toBe('Add Pet');
+
+    });
+
+    it('initial render with owners without pets and verify owner-3 details', () => {
+
+      const root = TestRenderer.create(<OwnerList/>).root;
+      const instance = root.instance;
+      instance.setState({
+        owners: [
+          { "firstName":"Jowena", "lastName":"Chua", "city":"Manila", "petIds":[],"id":-3 },
+          { "firstName":"Wally","lastName":"Crocker","city":"Sydney","petIds":[],"id":-2 },
+          { "firstName":"Monisha","lastName":"Suresh","city":"Bangalore","petIds":[],"id":-1 }
+        ],
+        pets: [],
+        isLoading: false
+      });
+
+      const ownerTableElement = root.findByProps({ className: "mt-4 owners" });
+      expect(ownerTableElement.type).toBe(Table);
+      const ownerItems = ownerTableElement.findByType('tbody').children;
+      expect(ownerItems.length).toBe(3);
+
+      const owner1NameTd = ownerItems[2].children[1].children[0];
+      expect(owner1NameTd).toBe("Monisha Suresh");
+      const owner1Details = ownerItems[2].findByProps({ className: "mt-4" });
+      expect(owner1Details.type).toBe(Table);      
+      const petsElementForOwner1 = owner1Details.findByType(PetList);
+      expect(petsElementForOwner1.children.length).toBe(1);
+      const addPetButton = petsElementForOwner1.findByType(Button);
+      expect(addPetButton.props.children).toBe('Add Pet');
 
     });
 
@@ -69,5 +129,6 @@ describe('components', () => {
       expect(instance.state.owners[0].petIds.length).toBe(2);
 
     });
+
   })
 });
