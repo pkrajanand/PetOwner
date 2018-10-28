8c45b0c2c5a2fd99504385cb2db0464134d824c4
RajPK <pkrajanand@gmail.com>
2018-10-28 13:53:35 +1100

    organised React custom components by grouping under src/components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import OwnerList from './OwnerList';
+import OwnerList from './components/OwnerList';
 
 class App extends Component {
 
